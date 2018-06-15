0f691eac21ded8fd4c3fb27f5fcf7785252c6bc3
Fabricio Sousa <magick.code@gmail.com>
2018-06-15 00:54:00 -0400

    feat: Create maps.html and app route decorator for testing

--- __init__.py
+++ __init__.py
@@ -350,7 +350,7 @@ def editPlace(place_id):
 # The Google Maps page route decorator
 @app.route('/map/')
 def showMap():    
-    return render_template('map.html')
+    return render_template('maps.html')
 
 
 # JSON APIs to feed Places info from database.
