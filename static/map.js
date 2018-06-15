4bec186a1c34445441d3f0e21e66463f1dde6160
Fabricio Sousa <magick.code@gmail.com>
2018-06-15 11:12:11 -0400

    fix: login success redirect

--- static/map.js
+++ static/map.js
@@ -143,51 +143,28 @@ function placetext(place) {
 var ViewModel = function() {
 
 	var self = this;
-    this.search = ko.observable("");
-    var data;
+	this.search = ko.observable("");
 
 	// Filter Places based on user input.
 	this.listPlaces = ko.computed(function() {
 		var search = self.search().toLowerCase();
 		if (!search) {
-            $.ajax({
-                dataType: "json",
-                url: 'http://ec2-18-220-254-32.us-east-2.compute.amazonaws.com/json/',
-                data: data,
-                success: function (data) {
-                    // begin accessing JSON data here
-                    // For each place in Places, call the geocodePlace function which geocodes the addresses.
-                    Places = data['places'];
-                    Places.forEach(function(place) {
-                        if (place.marker) {
-                            place.marker.setVisible(true);
-                        }
-                    });
-                    return Places;
-                }
-            });
-			
+			Places.forEach(function(place) {
+				if (place.marker) {
+					place.marker.setVisible(true);
+				}
+			});
+			return Places;
 		} else {
-            $.ajax({
-                dataType: "json",
-                url: 'http://ec2-18-220-254-32.us-east-2.compute.amazonaws.com/json/',
-                data: data,
-                success: function (data) {
-                    // begin accessing JSON data here
-                    // For each place in Places, call the geocodePlace function which geocodes the addresses.
-                    Places = data['places'];
-                    return ko.utils.arrayFilter(Places, function(place) {
-                        var match = place.name.toLowerCase().indexOf(search) !== -1;
-                        if (match) {
-                           place.marker.setVisible(true);
-                        } else {
-                           place.marker.setVisible(false);
-                        }
-                        return match;
-                    });
-                }
-            });
-			
+			return ko.utils.arrayFilter(Places, function(place) {
+		 		var match = place.name.toLowerCase().indexOf(search) !== -1;
+		 		if (match) {
+		 			place.marker.setVisible(true);
+		 		} else {
+		 			place.marker.setVisible(false);
+		 		}
+		 		return match;
+		 	});
 		}
 	});
 };
