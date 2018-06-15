1349b95374d9f3f357ec4d23ee517531ccd73f31
Fabricio Sousa <magick.code@gmail.com>
2018-06-15 13:54:10 -0400

    fix: __init__.py
    In the gconnect function, fixed an issue with the login call.

--- __init__.py
+++ __init__.py
@@ -60,95 +60,92 @@ def showLogin():
 
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
-    session['state'] = state
-    # return "The current session state is %s" % session['state']
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
 
 # gconnect route decorator.
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    """Login and/or register a user using Google OAuth."""
-    # Check if the posted STATE matches the session state
-    if request.args.get('state') != session['state']:
-        response = make_response(json.dumps('Invalid state parameter'), 401)
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
-
         return response
+    # Obtain authorization code
+    code = request.data
 
-    # Get the token sent through ajax
-    token = request.data
-
-    # Verify it
     try:
-        # Specify the CLIENT_ID of the app that accesses the backend:
-        idinfo = id_token.verify_oauth2_token(token, g_requests.Request(), CLIENT_ID)
-
-        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            # Wrong issuer
-            message = 'Wrong issuer.'
-            response = make_response(json.dumps(message), 401)
-            response.headers['Content-Type'] = 'application/json'
-
-            return response
-
-    except ValueError:
-        # Invalid token
-        message = 'Invalid token.'
-        response = make_response(json.dumps(message), 401)
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets(client_secret, scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
-
         return response
 
-    # ID token is valid. Get the user's Google Account ID from the decoded token.
-    gplus_id = idinfo['sub']
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
 
-    # Check that our client's id matches that of the token
-    # returned by Google API's server
-    if idinfo['aud'] != CLIENT_ID:
-        message = "Token's client ID does not match this app's."
-        response = make_response(json.dumps(message), 401)
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
-
         return response
 
-    # Verify that the user's NOT ALREADY LOGGED IN
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-    # Get the access token stored in the session if there is one
-    stored_token = session.get('token')
-    # Get the user id stored in the session if there is one
-    stored_gplus_id = session.get('gplus_id')
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-    # Check if there is already an access token in the session
-    # and if so, if the id of the token from the CREDENTIALS OBJECT
-    # matches the id stored in the session
-    if stored_token is not None and gplus_id == stored_gplus_id:
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
         response = make_response(json.dumps(
                   'Current user is already connected. '), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # If we get this far, the access token is VALID
-    # and it's THE RIGHT ACCESS TOKEN.
-    # The user can be successfully logged in
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
 
-    # 1. Store the access token in the session
-    session['token'] = token
-    session['gplus_id'] = gplus_id
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
 
-    # 3. Store user info in the session
-    session['username'] = idinfo['name']
-    session['picture'] = idinfo['picture']
-    session['email'] = idinfo['email']
+    data = answer.json()
 
-    # Specify we used Google to sign in
-    session['provider'] = 'google'
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    # ADD PROVIDER TO LOGIN SESSION
+    login_session['provider'] = 'google'
 
     # see if user exists, if it doesn't make a new one
-    user_id = getUserID(session['email'])
+    user_id = getUserID(login_session['email'])
     if not user_id:
-        user_id = createUser(session)
-    session['user_id'] = user_id
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
 
     output = ''
     output += '<img src="'
