be9b8e9c8496d9ba7a25bcf79b6aa733dd19c041
Fabricio Sousa <magick.code@gmail.com>
2018-06-15 01:24:38 -0400

    feat: Create new test app route decorator for testing creating a Places array from database

--- __init__.py
+++ __init__.py
@@ -256,11 +256,9 @@ def showPlaces():
                                places=places)
 
 
-# Main page route decorator showing all Places of Power
-@app.route('/text/')
+@app.route('/test/')
 def test():
     places = session.query(Place).order_by(desc(Place.date))
-
     return render_template('test.html',places=places)
 
 
