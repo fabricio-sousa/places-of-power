d45d2ec4c73f50a78e9c67ebdcec75a21d0adced
Fabricio Sousa <magick.code@gmail.com>
2018-06-16 19:13:33 -0400

    fix: Delete invalid place

--- __init__.py
+++ __init__.py
@@ -280,9 +280,9 @@ def addPlace():
 def showPlace(place_id):
     place = session.query(Place).filter_by(id=place_id).one()
     #if login_session['user_id'] != place.user_id:
-        return render_template('publicplace.html', place=place)
-    else:
-        return render_template('place.html', place=place)
+        #return render_template('publicplace.html', place=place)
+    #else:
+    return render_template('place.html', place=place)
 
 
 # Delete a Place of Power route decorator
