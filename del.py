d6549519966b6cae65e2fed0ce8e419cbe90344a
Fabricio Sousa <magick.code@gmail.com>
2018-06-17 16:56:45 -0400

    feat: Add db del admin feat

---
+++ del.py
@@ -0,0 +1,24 @@
+from db_setup import *
+from sqlalchemy import create_engine
+import datetime
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
+Base.metadata.bind = engine
+DBSession = scoped_session(sessionmaker(bind=engine))
+session = DBSession()
+
+
+place_del = session.query(Place).filter_by(id=6).one()
+session.delete(place_del)
+session.commit()
+
+'''
+user_del = session.query(User).filter_by(id=6).one()
+session.delete(place_del)
+session.commit()
+'''
+
+Print "Place and User deleted!"
+
+
