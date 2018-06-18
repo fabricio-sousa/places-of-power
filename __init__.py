5da3a2f53f5cec53d10a2ee76bc7cc47e10b8317
Fabricio Sousa <magick.code@gmail.com>
2018-06-18 10:39:56 -0400

    fix: Google Signin

--- __init__.py
+++ __init__.py
@@ -52,15 +52,17 @@ def login_required(f):
     return x
 
 # Create anti-forgery state token
-@app.route('/login')
-def showLogin():
-    state = ''.join(
-        random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+@app.route('/login/')
+def login():
+    """Route to the login page and create anti-forgery state token."""
+
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
     login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
-    return render_template('signin.html', STATE=state)
+    return render_template("login.html", STATE=state)
 
 
+# Connect to the Google Sign-in oAuth method.
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     # Validate state token
@@ -95,8 +97,8 @@ def gconnect():
         return response
 
     # Verify that the access token is used for the intended user.
-    gplus_id = credentials.id_token['sub']
-    if result['user_id'] != gplus_id:
+    google_id = credentials.id_token['sub']
+    if result['user_id'] != google_id:
         response = make_response(
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -106,23 +108,23 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
     stored_access_token = login_session.get('access_token')
-    stored_gplus_id = login_session.get('gplus_id')
-    if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+    stored_google_id = login_session.get('google_id')
+    if stored_access_token is not None and google_id == stored_google_id:
+        response = make_response(
+            json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
-    login_session['gplus_id'] = gplus_id
+    login_session['google_id'] = google_id
 
-    # Get user info
+    # Get user info.
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
@@ -132,13 +134,11 @@ def gconnect():
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-    # ADD PROVIDER TO LOGIN SESSION
-    login_session['provider'] = 'google'
 
-    # see if user exists, if it doesn't make a new one
-    user_id = getUserID(data["email"])
+    # See if the user exists. If it doesn't, make a new one.
+    user_id = get_user_id(data["email"])
     if not user_id:
-        user_id = createUser(login_session)
+        user_id = create_user(login_session)
     login_session['user_id'] = user_id
 
     output = ''
@@ -154,48 +154,73 @@ def gconnect():
 # User Helper Functions
 
 
-def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session[
-                   'email'], picture=login_session['picture'])
-    session.add(newUser)
+# Create new user.
+def create_user(login_session):
+    """Crate a new user.
+    Argument:
+    login_session (dict): The login session.
+    """
+
+    new_user = User(
+        name=login_session['username'],
+        email=login_session['email'],
+        picture=login_session['picture']
+    )
+    session.add(new_user)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
 
-def getUserInfo(user_id):
+def get_user_info(user_id):
+    """Get user information by ID.
+    Argument:
+        user_id (int): The user ID.
+    Returns:
+        The user's details.
+    """
+
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
-def getUserID(email):
+def get_user_id(email):
+    """Get user ID by email.
+    Argument:
+        email (str) : the email of the user.
+    """
+
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
     except:
         return None
 
-# DISCONNECT - Revoke a current user's token and reset their login_session
 
 
-@app.route('/gdisconnect')
+# Disconnect Google Account.
 def gdisconnect():
-    # Only disconnect a connected user.
+    """Disconnect the Google account of the current logged-in user."""
+
+    # Only disconnect the connected user.
     access_token = login_session.get('access_token')
     if access_token is None:
         response = make_response(
             json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
+
     if result['status'] == '200':
         response = make_response(json.dumps('Successfully disconnected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.'), 400)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -334,6 +359,10 @@ def disconnect():
         del login_session['user_id']
         del login_session['provider']
         flash("You have successfully logged out.")
+        return redirect(url_for('showMap'))
+    else:
+        flash("You were not logged in!")
+        return redirect(url_for('showMap'))
 
 
 if __name__ == '__main__':
